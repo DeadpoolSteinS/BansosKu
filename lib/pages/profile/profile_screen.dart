ce69b744f49cf6339ed95aacdc8347241e4e4fc6
DeadpoolSteinS <novaldisago@gmail.com>
2022-12-05 00:39:18 +0700

    finish profile page

--- lib/pages/profile/profile_screen.dart
+++ lib/pages/profile/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bansosku/constants/my_colors.dart';
+import 'package:bansosku/pages/profile/components/profile_option.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,6 +7,189 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 227,
+            decoration: const BoxDecoration(
+              color: MyColors.primaryGreen,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
+                )
+              ],
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              padding: const EdgeInsets.only(
+                top: 32,
+                right: 32,
+                bottom: 16,
+                left: 32,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 130,
+                        height: 42,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/BansosKu.png"),
+                          ),
+                        ),
+                      ),
+                      const Icon(
+                        Icons.logout,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        margin: const EdgeInsets.only(top: 50),
+                        padding: const EdgeInsets.only(
+                          top: 50,
+                          right: 16,
+                          bottom: 16,
+                          left: 16,
+                        ),
+                        decoration: BoxDecoration(
+                          color: MyColors.primaryBg,
+                          borderRadius: BorderRadius.circular(9),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                            )
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            const Text(
+                              "Jane Doe",
+                              style: TextStyle(
+                                color: MyColors.primaryGreen,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            Column(
+                              children: const [
+                                Text(
+                                  "Email",
+                                  style: TextStyle(
+                                    color: MyColors.primaryGreen,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  "janedoe@gmail.com",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 8),
+                            Column(
+                              children: const [
+                                Text(
+                                  "No. Handphone",
+                                  style: TextStyle(
+                                    color: MyColors.primaryGreen,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  "081237422327",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 12),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 48,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    side: const BorderSide(
+                                      color: MyColors.primaryGreen,
+                                      width: 2,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  backgroundColor: MyColors.primaryBg,
+                                ),
+                                onPressed: () {},
+                                child: const Text(
+                                  "Edit Profile",
+                                  style: TextStyle(
+                                    color: MyColors.primaryGreen,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Center(
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("assets/profile.png"),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  const ProfileOption(
+                    image: "setting3.png",
+                    label: "Riwayat Bansos",
+                  ),
+                  const SizedBox(height: 12),
+                  const ProfileOption(
+                    image: "setting2.png",
+                    label: "Atur Alamat",
+                  ),
+                  const SizedBox(height: 12),
+                  const ProfileOption(
+                    image: "setting1.png",
+                    label: "Atur E-Wallet, Debit, & Kredit",
+                  ),
+                  const SizedBox(height: 12),
+                  const ProfileOption(
+                    image: "setting4.png",
+                    label: "Ganti Password",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
